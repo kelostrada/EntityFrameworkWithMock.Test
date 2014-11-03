bd31cbc0f287da860254761d5d59894595f02762
Kelu <kelostrada@gmail.com>
2014-11-03 01:03:13 +0100

    Added test for ToList
    When using ToList method twice on the Blogs DbSet directly it gives
    different results (after first time it says the list is empty)

--- EntityFrameworkWithMock.Test/AsyncQueryTests.cs
+++ EntityFrameworkWithMock.Test/AsyncQueryTests.cs
@@ -12,6 +12,38 @@ namespace EntityFrameworkWithMock.Test
     public class AsyncQueryTests
     {
         [TestMethod]
+        public void SameBehaviourForTwoUsagesOfToList()
+        {
+            var data = new List<Blog> 
+            { 
+                new Blog { Name = "BBB" }, 
+                new Blog { Name = "ZZZ" }, 
+                new Blog { Name = "AAA" }, 
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Blog>>();
+            mockSet.As<IDbAsyncEnumerable<Blog>>()
+                .Setup(m => m.GetAsyncEnumerator())
+                .Returns(new TestDbAsyncEnumerator<Blog>(data.GetEnumerator()));
+
+            mockSet.As<IQueryable<Blog>>()
+                .Setup(m => m.Provider)
+                .Returns(new TestDbAsyncQueryProvider<Blog>(data.Provider));
+
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Blog>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<BloggingContext>();
+            mockContext.Setup(c => c.Blogs).Returns(mockSet.Object);
+
+            var list = mockContext.Object.Blogs.ToList();
+            var list2 = mockContext.Object.Blogs.ToList();
+
+            Assert.AreEqual(list.Count, list2.Count);
+        }
+
+        [TestMethod]
         public async Task GetAllBlogsAsync_orders_by_name()
         {
 
